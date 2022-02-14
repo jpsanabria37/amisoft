7a8e44ee90cb90e0797a45136d856497f35ee6e1
jpsanabria37 <jpsanabria37@misena.edu.co>
2022-02-13 20:47:17 -0500

    Proyecto y Solucion 1.2

---
+++ SolucionAmiSoft/BLL/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    internal class Person
+    {
+        private int id;
+        private string name;
+    }
+}

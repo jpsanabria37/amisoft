2a90c65b1ba04e153081f1e6b3662216aee9c572
jpsanabria37 <jpsanabria37@misena.edu.co>
2022-02-13 22:02:05 -0500

    Mi parte del trabajo 1.3

--- SolucionAmiSoft/BLL/Vehiculo.cs
+++ SolucionAmiSoft/BLL/Vehiculo.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class Vehiculo
+    public class Vehiculo
     {
+        private int id;
+        private Cliente Cliente;
+        private string placa;
+        private string color;
+        private string marca;
+        private string servicio;
+
+        public Vehiculo()
+        {
+
+        }
+
+        public Vehiculo(int id, string placa, string color, string marca, string servicio)
+        {
+            this.id = id;
+            this.placa = placa;
+            this.color = color;
+            this.marca = marca;
+            this.servicio = servicio;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Placa
+        {
+            get { return placa; }
+            set { placa = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public string Servicio
+        {
+            get { return servicio; }
+            set { servicio = value; }
+        }
+
     }
 }

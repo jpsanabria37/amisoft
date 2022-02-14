7395117b06cf952bd4ffb2c94b47b40b90937f81
jpsanabria37 <jpsanabria37@misena.edu.co>
2022-02-13 22:36:24 -0500

    se revirtieron los cambios

---
+++ SolucionAmiSoft/BLL/Rol.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Rol
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+
+        public Rol()
+        {
+        
+        }
+
+        public Rol(int id, string nombre, string descripcion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+        }
+        //Atributos
+        public int Id   
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre   
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        //Métodos
+    }
+}
---
+++ SolucionAmiSoft/BLL/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Usuario : Persona
+    {
+        private string password;
+        public Rol Rol;
+
+       
+
+	    public string Password
+	    {
+		    get { return password;}
+		    set { password = value;}
+	    }
+
+    }
+}
